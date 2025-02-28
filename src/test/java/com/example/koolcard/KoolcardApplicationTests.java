43cbcc53b50b0f75f8c2fbe3ae003d61ff7648a0
ghassenrached <ghassen.rached@etudiant-fsegt.utm.tn>
2025-02-28 10:27:35 +0100

    asslema

---
+++ src/test/java/com/example/koolcard/KoolcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.koolcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoolcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
